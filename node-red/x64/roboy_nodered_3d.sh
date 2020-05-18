abc36925965e1d03b062d01564f2c4f36c521dbd
Sausy <Michael.Sausmikat@gmx.at>
2020-05-18 13:31:05 +0200

    update visualization

--- node-red/x64/roboy_nodered_3d.sh
+++ node-red/x64/roboy_nodered_3d.sh
@@ -1,13 +1,41 @@
 mkdir -p docker-exchange/.node-red
 mkdir -p docker-exchange/controll
 
-git clone https://github.com/ahmadsayed/node-red-contrib-web-babylonjs.git
+git clone https://github.com/sausy/node-red-contrib-web-babylonjs.git
+
+docker rm -f mynodered > /dev/null
+docker rm -f myroscore > /dev/null
 
 docker-compose build
 
 #=== parameterisize it =====
+echo "======================="
+echo "build done"
+echo "======================="
 CURRETN_PATH=$(pwd)
-docker rm -f mynodered > /dev/null
-docker rm -f myroscore > /dev/null
-docker create -p 1880:1880 -p 9099:9099 --name mynodered -v ${CURRETN_PATH}/docker-exchange/.node-red:/data nodered/node-red:1.0.6-12-minimal-arm64v8
-docker create -p 11311:11311 --name myroscore -v ${CURRETN_PATH}/docker-exchange/controll:/data roscore/x64:melodic
+echo ${CURRETN_PATH}/docker-exchange/.node-red:/data
+#docker create -p 1880:1880 -p 9099:9099 --name mynodered -v ${CURRETN_PATH}/docker-exchange/.node-red:/data -v ${CURRETN_PATH}/docker-exchange/controll:/controll nodered/node-red:1.0.6-12-minimal-arm64v8
+docker create -p 1880:1880 -p 9099:9099 --name mynodered -v ${CURRETN_PATH}/docker-exchange/.node-red:/data -v ${CURRETN_PATH}/docker-exchange/controll:/controll nodered/node-red:x64
+docker create -p 11311:11311 --name myroscore roscore/x64:melodic
+
+docker start mynodered
+
+state_last=NULL
+
+while :
+do
+  state_current=$( cat $CURRETN_PATH/docker-exchange/controll/roscore )
+  if [ $state_last != $state_current ]
+    then
+    state_last=$state_current
+    if [ $state_current == 'false' ]
+      then
+      docker exec -it myroscore killall roscore
+      else
+      docker start myroscore
+
+    fi
+  fi
+
+	sleep 2
+done
