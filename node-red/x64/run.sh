503e8b47654ff77ab16bbd076f9f5a863601aa03
Sausy <michael.sausmikat@gmx.at>
2020-06-10 09:26:48 +0000

    updated run

--- node-red/x64/run.sh
+++ node-red/x64/run.sh
@@ -3,7 +3,6 @@
 
 #docker rm -f mynodered > /dev/null
 #docker rm -f myroscore > /dev/null
-#docker stop
 docker stop mynodered
 docker stop myroscore
 
@@ -22,11 +21,8 @@ echo "........................"
 echo "TIP: to change these vars"
 echo "edit the '.env' file"
 echo "======================="
-CURRETN_PATH=$(pwd)
-#echo ${CURRETN_PATH}/docker-exchange/.node-red:/data
-#docker create -p 1880:1880 -p 9099:9099 --name mynodered -v ${CURRETN_PATH}/docker-exchange/.node-red:/data -v ${CURRETN_PATH}/docker-exchange/controll:/controll nodered/node-red:1.0.6-12-minimal-arm64v8
-#docker create -p 1880:1880 -p 9099:9099 --name mynodered -v ${CURRETN_PATH}/docker-exchange/.node-red:/data -v ${CURRETN_PATH}/docker-exchange/controll:/controll nodered/node-red:${ARCHITECTUR}
-#docker create -p 11311:11311 -p 9090:9090 --name myroscore roscore/${ARCHITECTUR}:${ROSVERSION}
+#CURRETN_PATH=$(pwd)
+CURRETN_PATH=/home/${USER}//docker/node-red/x64
 
 unset ARCHITECTUR
 unset ROSVERSION
@@ -37,8 +33,6 @@ state_last=NULL
 state_last_int=NULL
 state_last_int2=NULL
 
-#killall roboy_lighthouse2
-
 while :
 do
   state_current=$( cat /home/${USER}/docker/node-red/x64/docker-exchange/controll/roscore )
@@ -48,15 +42,10 @@ do
       if [ $state_current == 'false' ]
         then
         echo "kill roscore"
-        #docker exec -it myroscore killall roslaunch
         docker stop myroscore
-	#killall roboy_lighthouse2
         docker stop myinterface
         else
         docker start myroscore
-	#& sleep 30 && \
-        #./roboy_lighthouse2
-
       fi
   fi
 
@@ -67,22 +56,12 @@ do
       if [ $state_current_int == 'false' ]
         then
         echo "stop udp to ros bridge"
-        #docker exec -it myroscore killall roslaunch
-        #docker stop myroscore
-        #killall roboy_lighthouse2
         docker stop myinterface
 	echo "...done"
         else
         echo "starting udp to ros bridge"
         docker start myinterface
         echo "...done"
-        #docker start myroscore 
-        #killall roboy_lighthouse2
-        # /home/ubuntu/start_udpinterface.sh & 
-        # /home/ubuntu/roboy_lighthouse2 &
-        #& sleep 30 && \
-        #./roboy_lighthouse2
-
       fi
   fi
 
@@ -92,19 +71,13 @@ do
       state_last_int2=$state_current_int2
       if [ $state_current_int2 == 'false' ]
         then
-        echo "kill lh2"
-        #docker exec -it myroscore killall roslaunch
-        #docker stop myroscore
-        killall roboy_lighthouse2_tracking
-        sleep 1
+        echo "stoping triangulation process"
+        docker stop mytriangulation
+        echo "...done"
         else
-        #docker start myroscore 
-        #killall roboy_lighthouse2
-        /home/ubuntu/roboy_lighthouse2_tracking & 
-        sleep 3
-        #& sleep 30 && \
-        #./roboy_lighthouse2
-
+	echo "starting triangulation process"
+	docker start mytriangulation
+        echo "...done"
       fi
   fi
 
