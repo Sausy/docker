dce9093fef7923fbc05e121c0c5ca0ad6285502b
Sausy <Sausmikat.Michael@gmail.com>
2020-07-27 19:16:38 +0000

    first working autostart

--- node-red/x64/build.sh
+++ node-red/x64/build.sh
@@ -5,11 +5,21 @@ git clone https://github.com/Sausy/node-red-contrib-web-babylonjs.git
 
 #docker run --rm --privileged multiarch/qemu-user-static --reset -p yes --credential yes
 
+echo "clear old builds"
+docker network disconnect rosnet myinterface
+docker stop mynodered
+docker stop myroscore
+docker stop myinterface
 docker rm -f mynodered > /dev/null
 docker rm -f myroscore > /dev/null
 docker rm -f myinterface > /dev/null
 
-docker-compose build
+read -p "(re)build docker images?" -n 1 -r
+if [[ ! $REPLY =~ ^[Nn]$ ]]
+then
+ echo "start building images"
+ docker-compose build
+fi
 
 export $(cat .env | awk 'NR == 1')
 export $(cat .env | awk 'NR == 2')
@@ -26,10 +36,18 @@ echo "======================="
 CURRETN_PATH=$(pwd)
 echo ${CURRETN_PATH}/docker-exchange/.node-red:/data
 #docker create -p 1880:1880 -p 9099:9099 --name mynodered -v ${CURRETN_PATH}/docker-exchange/.node-red:/data -v ${CURRETN_PATH}/docker-exchange/controll:/controll nodered/node-red:1.0.6-12-minimal-arm64v8
-docker create -p 1880:1880 -p 9099:9099 --name mynodered -v ${CURRETN_PATH}/docker-exchange/.node-red:/data -v ${CURRETN_PATH}/docker-exchange/controll:/controll nodered/node-red:${ARCHITECTUR}
-docker create -p 11311:11311 -p 9090:9090 --name myroscore roscore/${ARCHITECTUR}:${ROSVERSION}
-docker create -p 8000:8000 -p 4210:4210 -v ${CURRETN_PATH}/docker-exchange/src:/src -v ${CURRETN_PATH}/docker-exchange/lib:/src/lib -v ${CURRETN_PATH}/docker-exchange/usr/games:/usr/games -v ${CURRETN_PATH}/ros-entery/projectinterface:/entry --name myinterface rosbase/${ARCHITECTUR}:${ROSVERSION}
-docker create -v ${CURRETN_PATH}/docker-exchange/src:/src -v ${CURRETN_PATH}/docker-exchange/lib:/src/lib -v ${CURRETN_PATH}/docker-exchange/usr/games:/usr/games ${CURRETN_PATH}/ros-entery/projecttriangulation:/entry --name mytriangulation rosbase/${ARCHITECTUR}:${ROSVERSION}
+echo "Create virtual net for container"
+echo "virtual net for ros container communication"
+docker network rm rosnet
+docker network create --subnet=10.10.0.0/24 rosnet
+
+echo "Create containers that will start with device bootup"
+docker run -d --restart always -p 1880:1880 -p 9099:9099 --name mynodered -v ${CURRETN_PATH}/docker-exchange/.node-red:/data -v ${CURRETN_PATH}/docker-exchange/controll:/controll nodered/node-red:${ARCHITECTUR}
+docker run -d --restart always --net rosnet --ip 10.10.0.2 -p 9090:9090 -p 11311:11311 --name myroscore roscore/${ARCHITECTUR}:${ROSVERSION}
+#docker run -d --restart always --net rosnet --ip 10.10.0.10 -p 192.168.1.1:8002:8002/udp -p 8001:8001/udp -p 192.168.1.1:8000:8000/udp -p 4210:4210/udp -v ${CURRETN_PATH}/docker-exchange/src:/src -v ${CURRETN_PATH}/docker-exchange/lib:/src/lib -v ${CURRETN_PATH}/docker-exchange/usr/games:/usr/games -v ${CURRETN_PATH}/ros-entery/projectinterface:/entry --name myinterface rosbase/${ARCHITECTUR}:${ROSVERSION}
+#docker run -d --restart always -p 8002:8002 -p 8001:8001 -p 8000:8000 -p 4210:4210 -v ${CURRETN_PATH
+
+#docker create -v ${CURRETN_PATH}/docker-exchange/src:/src -v ${CURRETN_PATH}/docker-exchange/lib:/src/lib -v ${CURRETN_PATH}/docker-exchange/usr/games:/usr/games ${CURRETN_PATH}/ros-entery/projecttriangulation:/entry --name mytriangulation rosbase/${ARCHITECTUR}:${ROSVERSION}
 
 
 unset ARCHITECTUR
--- node-red/x64/docker-exchange/src/rosbase-entery.sh
+++ node-red/x64/docker-exchange/src/rosbase-entery.sh
@@ -2,7 +2,9 @@
 #roscore & sleep 4 && \
 #roslaunch rosbridge_server rosbridge_websocket.launch & sleep 4 && \
 #rosrun tf2_web_republisher tf2_web_republisher
-export ROS_MASTER_URI=http://192.168.1.1:11311
+#export ROS_MASTER_URI=http://192.168.1.1:11311
+DEFAULT_ROUTE=$(ip route show default | awk '/default/ {print $3}')
+export ROS_MASTER_URI=http://${DEFAULT_ROUTE}:11311
 source /opt/ros/melodic/setup.bash
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/src/lib
 
