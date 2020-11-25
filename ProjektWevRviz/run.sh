94abea151f0e2bbd798bb93051db88d0cb10d30f
Sausy <michael.sausmikat@gmx.at>
2020-11-25 20:54:15 +0100

    added easy startup script

--- ProjektWevRviz/run.sh
+++ ProjektWevRviz/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-IMAGE=rvizweb
+#IMAGE=rvizweb
 
-docker run -d --privileged --rm --name "sausy/rvizweb" -t "amd64" --network="host" "${IMAGE}"
+#docker run -d --privileged --rm --name "sausy/rvizweb" -t "amd64" --network="host" "${IMAGE}"
+docker run -d --privileged --rm --network="host" --name rvizweb -t rvizweb sausy/rvizweb:melodic
---
+++ run.sh
@@ -0,0 +1,10 @@
+cd ProjektDefault/rosDevBash/.
+./start.sh
+
+cd ../../ProjektRosCore/.
+./start.sh
+
+cd ../ProjektWevRviz/.
+./run.sh
+
+docker ps -a
