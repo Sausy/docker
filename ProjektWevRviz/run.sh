b31b0bbb573d012fca7be88b8c1fcce70dc14ea2
Sausy <Michael.Sausmikat@gmx.at>
2020-11-25 21:38:18 +0100

    bug fixfor webrviz

--- ProjektWevRviz/run.sh
+++ ProjektWevRviz/run.sh
@@ -3,4 +3,6 @@
 #IMAGE=rvizweb
 
 #docker run -d --privileged --rm --name "sausy/rvizweb" -t "amd64" --network="host" "${IMAGE}"
-docker run -d --privileged --rm --network="host" --name rvizweb -t rvizweb sausy/rvizweb:melodic
+docker stop rvizweb
+docker rm rvizweb
+docker run -d --rm --network="host" --name rvizweb sausy/rvizweb:melodic
