3cfbd0f64414d88586536f94b1964a59f2f41c93
Sausy <Michael.Sausmikat@gmx.at>
2020-11-25 21:16:15 +0100

    bug fix

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
+docker stop roscore
+docker stop devshell
+docker stop rvizweb
+
+docker pull sausy/roscore:amd64_melodic
+docker pull sausy/devshell:amd64
+docker pull sausy/rvizweb:melodic
+
 cd ProjektDefault/rosDevBash/.
 ./start.sh
 
